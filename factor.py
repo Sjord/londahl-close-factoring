f83faa1ca6110b23f4681fb3070f29de39790992
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2019-03-20 11:29:06 +0100

    Simplify and speed up check table loop

--- factor.py
+++ factor.py
@@ -15,16 +15,14 @@ def close_factor(n, b):
     # check the table
     mu = gmpy2.invert(pow(2, phi_approx, n), n)
     fac = pow(2, b, n)
-    j = 0
- 
-    while True:
+
+    for i in range(0, b + 1):
         mu = (mu * fac) % n
-        j += b
         if mu in look_up:
-            phi = phi_approx + (look_up[mu] - j)
+            phi = phi_approx + (look_up[mu] - i * b)
             break
-        if j > b * b:
-            return
+    else:
+        return None
  
     m = n - phi + 1
     roots = (m - gmpy2.isqrt(m ** 2 - 4 * n)) // 2, \
